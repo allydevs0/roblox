90ee7b8ab1828f47b024451f5cc6c3ec0edd0435
allydevs0 <allydevs0@gmail.com>
2025-02-24 17:58:21 -0300

    Update dex.lua

--- dex.lua
+++ dex.lua
@@ -1,16 +1,4 @@
-local Namecall
-Namecall = hookmetamethod(game, '__namecall', function(self, ...)
-   local Caller = tostring(getcallingscript())
-   local Method = getnamecallmethod()
-   
-   if Caller == 'ClientMover' and Method == 'GetService' then
-       return
-   end
-   
-   return Namecall(self, ...)
-end)
 
-warn('[Adonis Autobypass]: this game is running with autobypass!')
 local nodes = {}
 local selection
 local cloneref = cloneref or function(...) return ... end
